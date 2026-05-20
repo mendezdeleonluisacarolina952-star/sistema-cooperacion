f4ea2bbac31288eb934e7832be7ce674de8b1a9e
CarolUu <mendezdeleonluisacarolina952@gmail.com>
2026-05-19 19:32:49 -0600

    feat: estructura inicial del proyecto

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API funcionando correctamente",
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
